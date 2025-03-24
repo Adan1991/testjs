7f96e42d9d2b04ab151dc1dfb7b04cfe75762f99
Adan1991 <73646742+Adan1991@users.noreply.github.com>
2025-03-24 19:05:13 +0100

    Update dropdown.js

--- dropdown.js
+++ dropdown.js
@@ -2,7 +2,9 @@ document.addEventListener("DOMContentLoaded", function() {
     var clickable = document.getElementById("toggleText");
     var dropdown = document.getElementById("dropdownContent");
 
-    clickable.addEventListener("click", function() {
-        dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
-    });
+    if (clickable && dropdown) {
+        clickable.addEventListener("click", function() {
+            dropdown.classList.toggle("active");
+        });
+    }
 });
